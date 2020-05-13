60e4a22655cebf9004bc1801dbd231fb80a1271a
Sergei Bolotov <sbolotov@microsoft.com>
2020-05-13 12:52:38 -0700

    1348B

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -2,7 +2,5 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         main()
-
-        println(listOf(2, 4, 6, 8, 10, 12, 14, 16).reduce(Int::plus) - listOf(1, 3, 5, 7, 9, 11, 13, 23).reduce(Int::plus))
     }
 }
--- src/main/java/MainMain.kt
+++ src/main/java/MainMain.kt
@@ -1,25 +1,52 @@
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
 
 val scanner = Scanner(System.`in`)
 
 fun main() {
+    val arr = IntArray(10000)
+    val brr = IntArray(10000)
+    val cycle = ArrayList<Int>(10000)
+    val nums = HashSet<Int>(1000)
     scanner.apply {
         val t = nextInt()
 
         for (i in 1..t) {
+            cycle.clear()
+            nums.clear()
             val n = nextInt()
-            if (n % 4 != 0) {
-                println("NO")
-            } else {
-                println("YES")
-                for (j in 1..(n / 2)) {
-                    print("${j * 2} ")
+            val k = nextInt()
+            for (j in 0 until n) {
+                val x = nextInt()
+                arr[j] = x
+                if (x !in nums) {
+                    nums.add(x)
+                    cycle.add(x)
                 }
-                for (k in 1 until n / 2) {
-                    print("${k * 2 - 1} ")
-                }
-                println("${n + n / 2 - 1}")
             }
+            if (nums.size > k) {
+                println("-1")
+                continue
+            } else if (cycle.size < k) {
+                for (jj in cycle.size until k)
+                    cycle.add(1)
+            }
+            var l = 0
+            var size = 0
+            for (j in 0 until n) {
+                while (cycle[l] != arr[j]) {
+                    brr[size++] = cycle[l]
+                    l = (l + 1) % cycle.size
+                }
+                brr[size++] = arr[j]
+                l = (l + 1) % cycle.size
+            }
+            println(size)
+            for (j in 0 until size) {
+                print("${brr[j]} ")
+            }
+            println()
         }
     }
 }
