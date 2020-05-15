156b7443ee371b5bf02724e3a487a2b9ddc4d3ba
Sergei Bolotov <sbolotov@microsoft.com>
2020-05-15 12:24:20 -0700

    1350B

--- src/main/java/A.kt
+++ src/main/java/A.kt
@@ -1,77 +1,30 @@
 import java.util.*
+import kotlin.math.max
 
 private val scanner = Scanner(System.`in`)
-private val size = 2 * 100_000 + 10
+private val size = 100_000 + 10
 
 fun A() {
     scanner.apply {
-        val arr = IntArray(size)
-        val subarrs = TreeMap<Int, TreeMap<Int, Int>>()
         val t = nextInt()
         for (TEST in 1..t) {
+            val arr = IntArray(size)
+            val f = IntArray(size)
             val n = nextInt()
-            for (i in 0 until n) {
-                arr[i] = 0
+            for (i in 1..n) {
+                arr[i] = nextInt()
+                f[i] = 1
             }
-            subarrs[n] = TreeMap()
-            subarrs[n]?.set(1, n)
-            var action = 1
-            while (subarrs.isNotEmpty()) {
-                val largestArrs = subarrs[subarrs.lastKey()]!!
-                val firstArr = largestArrs.firstEntry()
-                largestArrs.remove(firstArr.key)
-                if (largestArrs.isEmpty()) {
-                    subarrs.remove(subarrs.lastKey())
+            for (i in 1..n) {
+                var j = i * 2
+                while (j <= n) {
+                    if (arr[i] < arr[j]) {
+                        f[j] = max(f[j], f[i] + 1)
+                    }
+                    j += i
                 }
-
-                val (l,r) = (firstArr.key to firstArr.value)
-                if ((r - l + 1) % 2 == 1) {
-                    arr[(l + r) / 2 - 1] = action
-                    val (ll, lr) = l to (l + r) / 2 - 1
-                    val (rl, rr) = (l + r) / 2 + 1 to r
-                    if (ll <= lr) {
-                        var tree = subarrs[lr - ll + 1]
-                        if (tree == null) {
-                            tree = TreeMap()
-                        }
-                        tree[ll] = lr
-                        subarrs[lr - ll + 1] = tree
-                    }
-                    if (rl <= rr) {
-                        var tree = subarrs[rr - rl + 1]
-                        if (tree == null) {
-                            tree = TreeMap()
-                        }
-                        tree[rl] = rr
-                        subarrs[rr - rl + 1] = tree
-                    }
-                } else {
-                    arr[(l + r - 1) / 2 - 1] = action
-                    val (ll, lr) = l to (l + r - 1) / 2 - 1
-                    val (rl, rr) = (l + r - 1) / 2 + 1 to r
-                    if (ll <= lr) {
-                        var tree = subarrs[lr - ll + 1]
-                        if (tree == null) {
-                            tree = TreeMap()
-                        }
-                        tree[ll] = lr
-                        subarrs[lr - ll + 1] = tree
-                    }
-                    if (rl <= rr) {
-                        var tree = subarrs[rr - rl + 1]
-                        if (tree == null) {
-                            tree = TreeMap()
-                        }
-                        tree[rl] = rr
-                        subarrs[rr - rl + 1] = tree
-                    }
-                }
-                action++
             }
-            for (i in 0 until n) {
-                print("${arr[i]} ")
-            }
-            println()
+            println(f.max())
         }
     }
 }
--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,6 +1,6 @@
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(SadJavaClass().solve(readLine()?.toInt() ?: 5))
+        A()
     }
 }
