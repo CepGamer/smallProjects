977dc8bc425a8132a706459316e6151408bcafc9
Sergei Bolotov <sbolotov@microsoft.com>
2020-05-19 09:17:45 -0700

    interbit

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,6 +1,43 @@
+internal class Interval {
+    var start: Int
+    var end: Int
+
+    constructor() {
+        start = 0
+        end = 0
+    }
+
+    constructor(s: Int, e: Int) {
+        start = s
+        end = e
+    }
+
+    override fun toString(): String {
+        return "[$start, $end]"
+    }
+}
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        B()
+        SadJavaClass().apply {
+            println(
+                merge(
+                    arrayListOf(
+                        Interval(27, 28),
+                        Interval(15, 46),
+                        Interval(5, 43),
+                        Interval(7, 15),
+                        Interval(46, 90),
+                        Interval(7, 64),
+                        Interval(18, 42),
+                        Interval(67, 84),
+                        Interval(48, 100),
+                        Interval(21, 27),
+                        Interval(26, 68)
+                    )
+                )
+            )
+        }
     }
 }
--- src/main/java/SadJavaClass.java
+++ src/main/java/SadJavaClass.java
@@ -1,18 +1,54 @@
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
+import java.util.*;
+
+/**
+ * Definition for an interval.
+ */
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + start + ", " + end + "]";
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
+
 
 public class SadJavaClass {
-    public int repeatedNumber(final List<Integer> A) {
-        BitSet set = new BitSet(A.size() + 1);
-        for (int a : A)
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        final PriorityQueue<Interval> queue = new PriorityQueue<>((o1, o2) ->
+                o1.start == o2.start ? o1.end - o2.end : o1.start - o2.start);
+        final ArrayList<Interval> result = new ArrayList<>();
+        queue.addAll(intervals);
+
+        if (queue.peek() == null)
+            return result;
+
+        Interval current = new Interval(queue.peek().start, queue.peek().end);
+        for (Interval interval = queue.poll();interval != null;interval = queue.poll())
         {
-            if (set.get(a))
+            if (current.end >= interval.start)
             {
-                return a;
+                current = new Interval(current.start, Math.max(current.end, interval.end));
             }
-            set.set(a);
+            else
+            {
+                result.add(current);
+                current = new Interval(interval.start, interval.end);
+            }
         }
-        return -1;
+        result.add(current);
+
+        return result;
     }
 }
