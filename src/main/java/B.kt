e80f279dd79dce65c1429bafc7d090b202bf5873
Sergei Bolotov <sbolotov@microsoft.com>
2020-05-21 10:29:01 -0700

    84C

--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -1,27 +1,58 @@
-import java.lang.StringBuilder
 import java.util.*
-import kotlin.math.min
 
 private val scanner = Scanner(System.`in`)
-private val size = 100_000 + 10
+private val size = 10_000 + 10
 
-fun main() {
+class Circle(val x: Int, val r: Int, val i: Int) {
+    var isHit = -1
+
+    fun tryHit(p: Pair<Int, Int>, shotNum: Int) {
+        val dx = p.first - x
+        val dy = p.second
+        if (isHit == -1 && dx * dx + dy * dy <= r * r) {
+            isHit = shotNum
+        }
+    }
+}
+
+fun findIfHit(arr: Array<Circle>, p: Pair<Int, Int>, n: Int, shotNum: Int) {
+    val x = p.first
+
+    var l = 0
+    var r = n - 1
+
+    while (r - l > 1) {
+        val m = (l + r) / 2
+        val c = arr[m]
+        if (c.x > x) {
+            r = m
+        } else {
+            l = m
+        }
+    }
+    arr[l].tryHit(p, shotNum)
+    arr[r].tryHit(p, shotNum)
+}
+
+fun B() {
     scanner.apply {
-        val arr = IntArray(size)
-        var dp = 1L
         val n = nextInt()
-        var res = 0L
+        val arr = Array(n) { Circle(0, 0, 0) }
         for (i in 0 until n) {
-            arr[i] = nextInt()
-            var dpi = 1L
-            if (i > 0 &&
-                arr[i] == arr[i - 1]) {
-                dpi = dp + 1L
-            }
-            dp = dpi
-            res += dp
+            arr[i] = Circle(nextInt(), nextInt(), i)
+        }
+        arr.sortBy { it.x }
+        val m = nextInt()
+        for (i in 1..m) {
+            val x = nextInt()
+            val y = nextInt()
+            findIfHit(arr, x to y, n, i)
         }
 
-        println(res)
+        arr.sortBy { it.i }
+        println(arr.count { it.isHit != -1 })
+        for (i in 0 until n) {
+            print("${arr[i].isHit} ")
+        }
     }
 }
