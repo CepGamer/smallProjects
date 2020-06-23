9d73b49c1748cbba21ddea7488a7bf2857284128
Sergei Bolotov <sbolotov@microsoft.com>
2020-06-23 11:32:26 -0700

    palindrome w/ const mem

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -13,16 +13,12 @@ enum class WhatToDo {
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val whatToDo = WhatToDo.A
+        val whatToDo = WhatToDo.JAVA
 
         when (whatToDo) {
             WhatToDo.JAVA ->
                 SadJavaClass().apply {
-                    println(firstMissingPositive(arrayListOf(8, 89)))
-                    println(firstMissingPositive((arrayListOf(3, 30, 34, 5, 9))))
-                    println(firstMissingPositive((arrayListOf(1, 2, 0, 5, 9))))
-                    println(firstMissingPositive((arrayListOf(3, 4, 34, 5, 1))))
-                    println(firstMissingPositive((arrayListOf(-3, -30, -34, -5, -9))))
+                    println(isPalindrome(2147447412))
                 }
             WhatToDo.A ->
                 A()
--- src/main/java/SadJavaClass.java
+++ src/main/java/SadJavaClass.java
@@ -1,17 +1,20 @@
-import java.util.ArrayList;
-import java.util.BitSet;
-
 public class SadJavaClass {
-    public int firstMissingPositive(ArrayList<Integer> A) {
-        BitSet set = new BitSet();
-        for (int a : A) {
-            if (a > 0)
-                set.set(a);
+    public int isPalindrome(int A) {
+        long a = A;
+        if (a < 0)
+            return 0;
+        long l = 1;
+        while (a / l >= 10) {
+            l *= 10L;
         }
-        for (int i = 1; i <= A.size() + 2; i++) {
-            if (!set.get(i))
-                return i;
+        long r = 10L;
+        while (l >= r) {
+            if ((a / l) % 10L != (a % r) / (r / 10)) {
+                return 0;
+            }
+            l /= 10L;
+            r *= 10L;
         }
-        return A.size() + 3;
+        return 1;
     }
 }
