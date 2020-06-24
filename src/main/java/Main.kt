b16333ee758830a736e9abb41c541c1390faf7f0
Sergei Bolotov <sbolotov@microsoft.com>
2020-06-24 14:25:19 -0700

    prettyprint

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -18,17 +18,8 @@ object Main {
         when (whatToDo) {
             WhatToDo.JAVA ->
                 SadJavaClass().apply {
-                    val func = { list: ArrayList<Int> ->
-                        arrange(list)
-                        println(list)
-                    }
-
-                    var a = arrayListOf(0, 1)
-                    func(a)
-                    a = arrayListOf(0, 1, 2)
-                    func(a)
-                    a = arrayListOf(1, 0)
-                    func(a)
+                    println(prettyPrint(3))
+                    println(prettyPrint(4))
                 }
             WhatToDo.A ->
                 A()
--- src/main/java/SadJavaClass.java
+++ src/main/java/SadJavaClass.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 
 public class SadJavaClass {
-    public void arrange(ArrayList<Integer> a) {
-        final int n = a.size();
-        for (int i = 0; i < n; i++) {
-            int ai = a.get(i);
-            a.set(i, (a.get(ai) % n) * n + a.get(i));
+    public ArrayList<ArrayList<Integer>> prettyPrint(int A) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        for (int i = 1; i <= A * 2 - 1; i++) {
+            ArrayList<Integer> line = new ArrayList<>();
+            for (int j = 1; j <= A * 2 - 1; j++) {
+                line.add(Math.max(Math.abs(i - A), Math.abs(j - A)) + 1);
+            }
+            res.add(line);
         }
-        for (int i = 0; i < n; i++) {
-            a.set(i, a.get(i) / n);
-        }
+        return res;
     }
 }
