beff2832fa78e3f366188fa9032ef32d07e26582
Sergei Bolotov <sbolotov@microsoft.com>
2020-07-01 09:19:33 -0700

    A & B

--- src/main/java/A.kt
+++ src/main/java/A.kt
@@ -7,49 +7,12 @@ import kotlin.math.min
 private val scanner = Scanner(System.`in`)
 private val size = 100_000 + 10
 
-
-fun printTable(n: Int, k: Int) {
-    val table = Array(n) { IntArray(n) }
-    var counter = 0
-    var x = 0
-    var y = 0
-    for (i in 0 until k) {
-        table[x++ % n][y++ % n] = 1
-        counter++
-        if (counter % n == 0) {
-            x++
-        }
-    }
-
-    val rmax = table.maxBy { it.sum() }?.sum() ?: 0
-    val rmin = table.minBy { it.sum() }?.sum() ?: 0
-    var cmax = 0
-    var cmin = 1000_000
-    for (i in 0 until n) {
-        var sum = 0
-        for (j in 0 until n) {
-            sum += table[i][j]
-        }
-        cmax = max(sum, cmax)
-        cmin = min(sum, cmin)
-    }
-
-    println((cmax - cmin) * (cmax - cmin) + (rmax - rmin) * (rmax - rmin))
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            print(table[i][j])
-        }
-        println()
-    }
-}
-
 fun A() {
     scanner.apply {
         val t = nextInt()
         for (TEST in 0 until t) {
             val n = nextInt()
-            val k = nextInt()
-            printTable(n, k)
+            println((n + 1) / 2)
         }
     }
 }
--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -1,40 +1,19 @@
 import java.util.*
 import kotlin.math.max
+import kotlin.math.min
 
 private val scanner = Scanner(System.`in`)
-
-fun res(success: Boolean) {
-    if (success) {
-        println("Yes")
-    } else {
-        println("No")
-    }
-}
-
-fun exec(a: Long, b: Long, n: Long, m: Long) {
-    if (a + b < n + m) {
-        return res(false)
-    }
-
-    if (m <= a && m <= b) {
-        return res(true)
-    } else if (n >= listOf(a, b, m).max() ?: 0) {
-        return res(true)
-    } else {
-        return res(false)
-    }
-}
 
 fun B() {
     scanner.apply {
         val t = nextInt()
         for (TEST in 0 until t) {
-            val a = nextLong()
-            val b = nextLong()
             val n = nextLong()
-            val m = nextLong()
+            val r = nextLong()
 
-            exec(a, b, n, m)
+            val min = min(n - 1, r)
+            val surplus = if (r >= n) 1 else 0
+            println(min * (min + 1) / 2 + surplus)
         }
     }
 }
