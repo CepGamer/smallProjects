9d666862f4659a5951fbadef75089dd3546dc803
Sergei Bolotov <sbolotov@microsoft.com>
2020-10-29 14:17:13 -0700

    36A

--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -1,12 +1,58 @@
+import java.io.File
 import java.util.*
-import kotlin.math.min
+
 
 private val scanner = Scanner(System.`in`)
 
 fun B() {
-    scanner.apply {
-        for (i in 0 until 10_000) {
-            println("$i ${i + 1}")
+    val res = solve()
+    val out = File("output.txt")
+    out.createNewFile()
+    out.writeText(if (res) "YES" else "NO")
+}
+
+fun solve(): Boolean {
+    val file = File("input.txt")
+    val sc = Scanner(file)
+
+    sc.apply {
+        val n = nextInt()
+        nextLine()
+        val s = nextLine()
+        var hasBegun = false
+        var cycleInitiated = false
+        var cyclePeriod = 0
+        var curCycle = 0
+        var hasEnded = false
+        for (c in s) {
+            if (!hasBegun) {
+                hasBegun = c == '1'
+                continue
+            }
+            else if (!cycleInitiated) {
+                if (c == '1') {
+                    cycleInitiated = true
+                    curCycle = cyclePeriod
+                } else {
+                    cyclePeriod++
+                }
+            } else if (!hasEnded) {
+                if (c == '1') {
+                    if (curCycle == 0) {
+                        curCycle = cyclePeriod
+                    } else {
+                        return false
+                    }
+                } else if (curCycle == 0) {
+                    hasEnded = true
+                } else {
+                    curCycle--
+                }
+            } else if (hasEnded && c == '1') {
+                return false
+            }
         }
+
+        return true
     }
 }
