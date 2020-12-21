e853b675b2a174542b611e4e53f4a9e1342b3546
Sergei Bolotov <sbolotov@microsoft.com>
2020-12-21 14:30:24 -0800

    c692E

--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -11,7 +11,8 @@ private var n: Int = 0
 
 fun B() {
     scanner.apply {
-        val T = nextInt()
+        val T = 1//nextInt()
+
         for (TEST in 1..T) {
             initB()
             runTestB()
@@ -25,10 +26,64 @@ fun initB() {
 fun runTestB() {
     scanner.apply {
         n = nextInt()
-        val m = nextInt()
-        for (i in 1..m) {
-            val x = nextInt()
-            val y = nextInt()
+        val m = nextLong()
+        nextLine()
+        val s = nextLine()
+
+        println(if (solveB(m.toLong(), s)) "Yes" else "No")
+    }
+}
+
+fun solveB(number: Long, string: String): Boolean {
+    val digits = IntArray(26)
+    var res = 0L
+
+    res += pow(2L, string[string.length - 1] - 'a')
+    res -= pow(2L, string[string.length - 2] - 'a')
+
+    if (string.length == 2) {
+        return res == number
+    }
+
+    for (c in string.substring(0, string.length - 2)) {
+        digits[c - 'a']++
+    }
+
+    for (i in 25 downTo 0) {
+        var x = digits[i]
+        while (x > 0) {
+            val value = pow(2L, i)
+            if (res > number) {
+                res -= value
+            } else {
+                res += value
+            }
+
+            x--
         }
     }
+
+    return res == number
+}
+
+fun pow(x: Long, deg: Int): Long {
+    var dg = deg
+    var res = x
+    var tail = 1L
+    if (deg == 0) {
+        return 1
+    } else if (deg == 1) {
+        return x
+    }
+
+    while (dg > 0) {
+        if (dg % 2L == 1L) {
+            tail *= res
+        }
+
+        res *= res
+        dg /= 2
+    }
+
+    return tail
 }
