69c0071dd24a97c4ff0a8a40b1da2470a3411e9b
Sergei Bolotov <sbolotov@microsoft.com>
2020-12-30 14:53:49 -0800

    slightly faster E

--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -27,6 +27,7 @@ fun runTestB(T: Int, t: Int) {
         val modulo = 1_000_000_000L + 7L
         val bitsCount = LongArray(60)
         val pow = LongArray(60) { 1 }
+        val bitsByPow = LongArray(60) { 1 }
         for (i in 1..59) {
             pow[i] = pow[i - 1] * 2L % modulo
         }
@@ -39,17 +40,20 @@ fun runTestB(T: Int, t: Int) {
             }
         }
 
+        for (i in 0..59) {
+            bitsByPow[i] = pow[i] * bitsCount[i]
+        }
+
         var res = 0L
-        for (i in 0 until n) {
+        for (x in vals) {
             var sumOr = 0L
             var sumAnd = 0L
-            val x = vals[i]
             for (j in 0..59) {
                 if (x and (1L shl j) != 0L) {
-                    sumAnd = (sumAnd + (pow[j]) * bitsCount[j]) % modulo
+                    sumAnd = (sumAnd + bitsByPow[j]) % modulo
                     sumOr = (sumOr + (pow[j]) * n) % modulo
                 } else {
-                    sumOr = (sumOr + (pow[j]) * bitsCount[j]) % modulo
+                    sumOr = (sumOr + bitsByPow[j]) % modulo
                 }
             }
             res = (res + (sumAnd * sumOr)) % modulo
