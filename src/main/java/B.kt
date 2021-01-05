7ae11ea2fcb267bfd18fc330fc302ef474cedeac
Sergei Bolotov <sbolotov@microsoft.com>
2021-01-05 15:57:58 -0800

    RIDICULOUS time limits on F

--- src/main/java/B.kt
+++ src/main/java/B.kt
@@ -1,9 +1,17 @@
+import java.io.BufferedInputStream
+import java.lang.StringBuilder
 import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-private val scanner = Scanner(System.`in`)
+private val scanner = FastScanner()
 private val size = 100_000 + 10
 
 private var n: Int = 0
+
+private lateinit var Int: BooleanArray
+private lateinit var edges: HashMap<Int, ArrayDeque<Int>>
 
 var debug = false
 
@@ -18,49 +26,108 @@ fun B() {
     }
 }
 
+class FastScanner {
+    private val BS = 1 shl 16
+    private val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+    private var `in`: BufferedInputStream? = null
+
+    private val char: Char
+        get() {
+            while (bId == size) {
+                size = try {
+                    `in`!!.read(buf)
+                } catch (e: Exception) {
+                    return NC
+                }
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+
+    init {
+        `in` = BufferedInputStream(System.`in`, BS)
+    }
+}
+
 fun initB() {
+    edges = HashMap()
 }
 
 fun runTestB(T: Int, t: Int) {
     scanner.apply {
         n = nextInt()
-        val x = nextInt()
-        var sum = 0L
-        val array = IntArray(n)
+        val m = nextInt()
 
-        var i = 0
-        var stopped = false
-        var multiple = 1
-        for (j in 0 until n) {
-            val num = nextInt()
-            array[j] = num
+        val visited = IntArray(n + 1)
+
+        for (i in 1..m) {
+            val a = nextInt()
+            val b = nextInt()
+            if (!edges.containsKey(a)) {
+                edges[a] = ArrayDeque()
+            }
+            edges[a]!!.add(b)
+            if (!edges.containsKey(b)) {
+                edges[b] = ArrayDeque()
+            }
+            edges[b]!!.add(a)
         }
 
-        while (true) {
-            for (j in 0 until n) {
-                val num = array[j]
-                sum += num * multiple
-                i = j
-                if (!stopped && num % x == 0) {
-                    array[j] = num / x
+        val next = ArrayDeque<Int>(n + 1)
+        next.addLast(1)
+        var blacks = 0
+        var seen = 0
+        val result = ArrayList<Int>(n)
+        while (!next.isEmpty()) {
+            val v = next.removeFirst()
+            if (visited[v] and 2 != 0) {
+                continue
+            }
+            visited[v] = visited[v] or 2
+            val isCurrentWhite = visited[v] and 1 != 0
+            if (!isCurrentWhite) {
+                result.add(v)
+                blacks++
+            }
+            seen++
+            if (!edges.containsKey(v))
+                break
+            val neighbours = edges[v]!!
+            while (!neighbours.isEmpty()) {
+                val u = neighbours.removeFirst()
+                if (!isCurrentWhite) {
+                    visited[u] = visited[u] or 1
+                    next.addLast(u)
                 } else {
-                    stopped = true
-                    break
+                    next.addFirst(u)
                 }
             }
+        }
 
-            if (stopped) {
-                for (j in (i + 1) until n) {
-                    sum += array[j] * multiple
-                }
-                multiple *= x
-                for (j in 0 until i) {
-                    sum += array[j] * multiple
-                }
-                println(sum)
-                return
-            }
-            multiple *= x
+        if (seen == n) {
+            println("YES\n$blacks\n" + result.joinToString(" "))
+        } else {
+            println("NO")
         }
     }
 }
---
+++ src/main/java/FastScanner.kt
@@ -0,0 +1,44 @@
+import java.io.BufferedInputStream
+
+class FastScannerForCopy {
+    private val BS = 1 shl 16
+    private val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+    private var `in`: BufferedInputStream? = null
+
+    private val char: Char
+        get() {
+            while (bId == size) {
+                size = try {
+                    `in`!!.read(buf)
+                } catch (e: Exception) {
+                    return NC
+                }
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+
+    init {
+        `in` = BufferedInputStream(System.`in`, BS)
+    }
+}
