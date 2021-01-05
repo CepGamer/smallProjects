084af098b92e1055c4d7817274a9b9e58206ea7f
Sergei Bolotov <sbolotov@microsoft.com>
2021-01-05 09:41:51 -0800

    decision of D for historical purposes

--- src/main/java/A.kt
+++ src/main/java/A.kt
@@ -1,6 +1,8 @@
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import kotlin.math.max
+import kotlin.math.min
 
 var enableDebug = false
 private val scanner = Scanner(System.`in`)
@@ -24,17 +26,107 @@ fun init() {
 fun runTest(test:Int, t: Int) {
     scanner.apply {
         n = nextInt()
-        val x = nextInt()
-        var sumDivided = 0
-        var totalSum = 0L
+        val array = IntArray(n)
+        val maps = HashMap<Int, ArrayList<Pair<Int, HashMap<Int, Int>>>>()
+        val mapArray = ArrayList<HashMap<Int, Int>>()
         for (i in 0 until n) {
-            val y = nextInt()
-            totalSum += y
-            val divRes = y / x + (if (y % x == 0) 0 else 1)
-            sumDivided += divRes
+            array[i] = nextInt()
+            var num = array[i]
+            val map = HashMap<Int, Int>()
+            var cnt2 = 0
+            while (num % 2 == 0) {
+                cnt2++
+                num /= 2
+            }
+            if (cnt2 != 0) {
+                map[2] = cnt2
+            }
+            var mul = 3
+            while (mul * mul <= num) {
+                var cnt = 0
+                while (num % mul == 0) {
+                    cnt++
+                    num /= mul
+                }
+                if (cnt != 0) {
+                    map[mul] = cnt
+                }
+
+                mul += 2
+            }
+
+            if (num != 1) {
+                map[num] = 1
+            }
+
+            for (k in map.keys) {
+                if (!maps.containsKey(k)) {
+                    maps[k] = ArrayList()
+                }
+                maps[k]!!.add(i to map)
+            }
+            mapArray.add(map)
+        }
+        val q = nextInt()
+        for (i in 0 until q) {
+            nextLong()
         }
 
-        println("${totalSum / x + (if (totalSum % x == 0L) 0 else 1)} $sumDivided")
+        var maxAdj = 1
+        val marked = BooleanArray(n)
+        for (i in 0 until n) {
+            if (marked[i]) {
+                continue
+            }
+            var curAdj = 1
+            val map = mapArray[i]
+            for (k in map.keys) {
+                val matching = maps[k]!!
+
+                for ((j, matchingMap) in matching) {
+                    if (j == i || marked[j]) continue
+
+                    var isMatching = true
+                    for (matchingKey in matchingMap.keys) {
+                        if (!map.containsKey(matchingKey)) {
+                            isMatching = false
+                            break
+                        }
+
+                        val a = map[matchingKey]!!
+                        val b = matchingMap[matchingKey]!!
+                        val mx = max(a, b)
+                        val mn = min(a, b)
+                        if ((mx - mn) % 2 != 0) {
+                            isMatching = false
+                            break
+                        }
+                    }
+
+                    for (nonMatchingKey in map.keys) {
+                        if (matchingMap.containsKey(nonMatchingKey))
+                            continue
+                        else if (map[nonMatchingKey]!! % 2 != 0) {
+                            isMatching = false
+                            break
+                        }
+                    }
+
+                    if (isMatching) {
+                        curAdj++
+                        marked[j] = true
+                    }
+                }
+            }
+
+            maxAdj = max(curAdj, maxAdj)
+
+            marked[i] = true
+        }
+
+        for (i in 1..q) {
+            println(maxAdj)
+        }
     }
 }
 
