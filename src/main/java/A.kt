ca85a480735fc150bd516a7e1b4644ca514ef73a
Sergei Bolotov <sbolotov@microsoft.com>
2021-01-19 06:29:28 -0800

    D UNFINISHED TODO

--- src/main/java/A.kt
+++ src/main/java/A.kt
@@ -1,34 +1,53 @@
 import java.lang.StringBuilder
 import java.util.*
-import kotlin.math.max
 
 var enableDebug = false
 private val scanner = Scanner(System.`in`)
 private val size = 100_000 + 10
 
 private var n: Int = 0
-private val N = 1_000_000 + 10
+private val N = 5_000 + 1
 private val array = IntArray(N)
 private val count = IntArray(N)
 private val isEqual0 = BooleanArray(N)
 
 val modulo = 1000_000_000L + 7L
 
+val paths = Array(N) { LongArray(N) }
+val times = LongArray(N)
+
 fun A() {
     scanner.apply {
         preInit()
-        val T = nextInt()
+        val T = 1//nextInt()
         for (TEST in 1..T) {
-            init()
             println(runTest(TEST, T))
         }
     }
 }
 
 fun preInit() {
+    for (i in 0 until N) {
+        paths[i][0] = 1
+    }
 }
 
-fun init() {
+fun init(k: Int) {
+    for (i in 1 until N) {
+        paths[0][i] = paths[1][i - 1]
+        paths[n - 1][i] = paths[n - 2][i - 1]
+        for (j in 1 until (n - 1)) {
+            paths[j][i] = (paths[j - 1][i - 1] + paths[j + 1][i - 1]) % modulo
+            if (paths[j][i] < 0) paths[j][i] += modulo
+        }
+    }
+    for (i in 0 until n) {
+        for (j in 0..k) {
+            times[i] += paths[i][j] * paths[i][k - j]
+        }
+        times[i] %= modulo
+        if (times[i] < 0) times[i] += modulo
+    }
 }
 
 fun runTest(test: Int, t: Int): String {
@@ -37,22 +56,31 @@ fun runTest(test: Int, t: Int): String {
         val k = nextInt()
         val q = nextInt()
 
+        init(k)
+
+        var res = 0L
         val arr = LongArray(n)
         for (i in 0 until n) {
             arr[i] = nextLong()
+            res += arr[i] * times[i]
+            res %= modulo
+            if (res < 0) res += modulo
         }
 
-        val times = Array(n) { LongArray(k) }
-
-        for (i in 0 until n) {
-            var sum = arr[i]
-            times[i][0] = sum
-            for (j in 1 until k) {
-
+        val stringBuilder = StringBuilder()
+        for (i in 0 until q) {
+            val (j, a) = nextInt() - 1 to nextLong()
+            res -= (arr[j] * times[j])
+            arr[j] = a
+            res += a * times[j]
+            res %= modulo
+            if (res < 0) {
+                res += modulo
             }
+            stringBuilder.appendLine(res)
         }
 
-        return ""
+        return stringBuilder.toString()
     }
 }
 
