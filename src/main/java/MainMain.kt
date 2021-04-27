94605e34e64b46b5881554ed947e1e49d2af480c
Sergei Bolotov <sbolotov@microsoft.com>
2021-04-26 17:31:36 -0700

    LC bs

--- src/main/java/MainMain.kt
+++ src/main/java/MainMain.kt
@@ -1,28 +1,13 @@
-import java.lang.AssertionError
-import java.lang.StringBuilder
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
-import kotlin.math.max
-
-private val scanner = Scanner(System.`in`)
-
-class TreeNode(var `val`: Int) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
-}
 fun main() {
     val sln = Solution()
 
-    asrt(sln.maximumSwap(2736), 7236)
-    asrt(sln.maximumSwap(9973), 9973)
-    asrt(sln.maximumSwap(1), 1)
-    asrt(sln.maximumSwap(10), 10)
-    asrt(sln.maximumSwap(101), 110)
-    asrt(sln.maximumSwap(12345), 52341)
-    asrt(sln.maximumSwap(98368), 98863)
-    asrt(sln.maximumSwap(0), 0)
-    asrt(sln.maximumSwap(100000010), 110000000)
+    asrt(sln.minEatingSpeed(intArrayOf(1, 2), 2), 2)
+    asrt(sln.minEatingSpeed(intArrayOf(1, 5), 2), 5)
+    asrt(sln.minEatingSpeed(intArrayOf(1, 5), 5), 2)
+    asrt(sln.minEatingSpeed(intArrayOf(1, 5), 6), 1)
+    asrt(sln.minEatingSpeed(intArrayOf(3, 6, 7, 11), 8), 4)
+    asrt(sln.minEatingSpeed(intArrayOf(30, 11, 23, 4, 20), 5), 30)
+    asrt(sln.minEatingSpeed(intArrayOf(30, 11, 23, 4, 20), 6), 23)
 }
 
 fun asrt(a: Int, b: Int) {
@@ -30,73 +15,25 @@ fun asrt(a: Int, b: Int) {
 }
 
 class Solution {
-    fun findTargetSumWays(nums: IntArray, target: Int): Int {
-        var sums = IntArray(2000 + 10)
-        var sums2 = IntArray(2000 + 10)
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        val maxSpeed = piles.maxOrNull() ?: 1000_000_000
 
-        sums[nums[0] + 1000]++
-        sums[-nums[0] + 1000]++
-        for (ai in 1 until nums.size) {
-            val a = nums[ai]
-            for (i in 0..2001) {
-                if (sums[i] != 0) {
-                    sums2[a + i] += sums[i]
-                    sums2[-a + i] += sums[i]
-                }
+        var (l, r) = 1 to maxSpeed + 1
+
+        while (l + 1 <= r) {
+            val m = (l + r) / 2
+
+            if (countTime(piles, m) <= h) {
+                r = m
+            } else {
+                l = m + 1
             }
-
-            val arr = sums
-            sums = sums2
-            sums2 = arr
-            sums2.fill(0)
         }
 
-        return sums[target + 1000]
+        return l
     }
 
-    fun maximumSwap(num: Int): Int {
-        var a = 1
-        var dgs = 0
-        val string = IntArray(10)
-        val maxVal = IntArray(10)
-        while (a < num) {
-            string[dgs] = getDigit(num, dgs++)
-            a *= 10
-        }
-
-        maxVal[0] = string[0]
-        for (i in 1 until 10) {
-            maxVal[i] = max(maxVal[i - 1], string[i])
-        }
-
-        string[dgs] = getDigit(num, dgs--)
-
-        var i = dgs
-        while (i >= 1) {
-            if (string[i] < maxVal[i]) {
-                for (j in 0 until i) {
-                    if (string[j] == maxVal[i]) {
-                        val a = string[i]
-                        string[i] = string[j]
-                        string[j] = a
-                    }
-                }
-                break
-            }
-            i--
-        }
-
-        return string.foldRight(0) { a, acc -> acc * 10 + a }
-    }
-
-    fun getDigit(num: Int, digit: Int): Int {
-        var dg = digit
-        var n = num
-        while (dg > 0) {
-            dg--
-            n /= 10
-        }
-
-        return n % 10
+    fun countTime(piles: IntArray, speed: Int): Int {
+        return piles.map { it / speed + (if (it % speed > 0) 1 else 0) }.sum()
     }
 }
